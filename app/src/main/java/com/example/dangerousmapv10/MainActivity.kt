952f2557fec2e1e51967ac2284a6b0d1975426e4
Yehia <yihea2000@gmail.com>
2023-06-20 21:40:12 +0300

    To Cairo

--- app/src/main/java/com/example/dangerousmapv10/MainActivity.kt
+++ app/src/main/java/com/example/dangerousmapv10/MainActivity.kt
@@ -1,37 +1,38 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.dangerousmapv10
 
-import android.Manifest
-import android.content.Context
-import android.content.pm.PackageManager
-import android.graphics.Color.WHITE
+
 import android.location.LocationManager
-import android.os.Build
 import android.os.Bundle
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.app.ActivityCompat
+import com.example.dangerousmapv10.ui.theme.Black
 import com.example.dangerousmapv10.ui.theme.DangerousMapV10Theme
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapType
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+import kotlinx.coroutines.launch
+
 
 //import androidx.compose.ui.text.input.PasswordTextField
 
@@ -47,37 +48,92 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.primary
                 ) {
-                    LoginPage()
-                    //Map()
+                    //LoginPage()
+                    Map()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Map() {
 
+@Composable
+
+fun Map() {
+    var uiSettings by remember { mutableStateOf(MapUiSettings(zoomControlsEnabled = false)) }
+    var properties by remember { mutableStateOf(MapProperties(mapType = MapType.NORMAL)) }
     val singapore = LatLng(1.35, 103.87)
-    val sydney = LatLng(-33.852, 151.211)
+    val cairo = LatLng(30.0444, 31.2357)
+    val cairoState = MarkerState(position = cairo)
+    var currLatLong by remember {
+        mutableStateOf(LatLng(1.35, 103.87))
+    }
+    var addMarkerBoolean by remember {
+        mutableStateOf(false)
+    }
 
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(singapore, 10f)
     }
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+    ) {
 
-    Box(Modifier.fillMaxSize()) {
-        GoogleMap(
-            modifier = Modifier.matchParentSize(),
-            cameraPositionState = cameraPositionState
-        )
-        Button(
-            onClick = {
-                cameraPositionState.move(CameraUpdateFactory.newLatLng(sydney))
+
+        Box(Modifier.fillMaxSize()) {
+
+            GoogleMap(
+                modifier = Modifier.matchParentSize(),
+                cameraPositionState = cameraPositionState,
+                uiSettings = uiSettings,
+                properties = properties,
+                onMapClick = {
+                    currLatLong = it
+                    addMarkerBoolean = true
+
+                }
+
+            ) {
+                if (addMarkerBoolean) {
+                    Marker(state = MarkerState(currLatLong), title = "I did it")
+
+
+                }
+
+
+                Marker(
+                    state = cairoState,
+                    title = "this is cairo"
+                )
             }
-        ) {
-            Text(text = "Animate camera to Sydney")
+            val coroutinScope = rememberCoroutineScope()
+            Button(
+                modifier = Modifier.align(Alignment.BottomCenter),
+
+                onClick = {
+                    coroutinScope.launch {
+                        cameraPositionState.animate(
+                            CameraUpdateFactory.newLatLng(
+                                cairo
+                            )
+                        )
+                    }
+
+                }
+            ) {
+                Text(text = "Animate camera to Cairo", color = Color.White)
+            }
+
         }
     }
+
+}
+
+@Composable
+fun addMarker(myloc: MarkerState) {
+
+    Marker(state = myloc, title = "onClick")
+
 }
 
 @Composable
@@ -114,6 +170,7 @@ fun LoginPage() {
                     onValueChange = { email = it },
                     label = { Text("Email") },
                     colors = TextFieldDefaults.textFieldColors(
+                        textColor = Black,
                         containerColor = Color(0xFFD3D3D3),
                         unfocusedLabelColor = Color(0xFF696969),
                         focusedIndicatorColor = Color.Transparent,
@@ -134,6 +191,7 @@ fun LoginPage() {
                     label = { Text("Password") },
                     visualTransformation = PasswordVisualTransformation(),
                     colors = TextFieldDefaults.textFieldColors(
+                        textColor = Black,
                         containerColor = Color(0xFFD3D3D3),
                         unfocusedLabelColor = Color(0xFF696969),
                         focusedIndicatorColor = Color.Transparent,
@@ -144,8 +202,10 @@ fun LoginPage() {
         }
         Button(
             onClick = { /* perform login */ },
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF1E3C72),
-            contentColor = Color.White),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF1E3C72),
+                contentColor = Color.White
+            ),
             modifier = Modifier
                 .align(Alignment.BottomCenter)
                 .padding(16.dp)
